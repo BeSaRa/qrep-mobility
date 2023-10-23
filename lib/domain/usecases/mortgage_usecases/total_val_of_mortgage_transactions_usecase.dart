80daf28b972353c166f8cb0d383b26d98cd3bb2d
eyad.alsawah <eyad.alsawah@nasca-sy.com>
2023-10-23 13:25:44 +0300

    mortgage grid usecases

--- lib/domain/usecases/mortgage_usecases/total_val_of_mortgage_transactions_usecase.dart
+++ lib/domain/usecases/mortgage_usecases/total_val_of_mortgage_transactions_usecase.dart
@@ -1,1 +1,21 @@
+import 'package:ebla/data/newtwok/failure_model/failure.dart';
+import 'package:ebla/domain/models/rent_models/rent_models.dart';
 
+import 'package:ebla/domain/models/requests/sell_requests/request_sell_values.dart';
+import 'package:ebla/domain/repository/repository.dart';
+import 'package:ebla/domain/usecases/base_usecases/base_usecase.dart';
+
+import 'package:multiple_result/multiple_result.dart';
+
+class TotalValOfMortgageTransactionsUsecase
+    implements BaseUseCase<RequestSellValues, List<BaseRentResponse>> {
+  final Repository repository;
+
+  TotalValOfMortgageTransactionsUsecase(this.repository);
+
+  @override
+  Future<Result<List<BaseRentResponse>, FailureModel>> execute(
+      RequestSellValues input) async {
+    return await repository.getTotalValueOfMortgageTransactions(input);
+  }
+}
