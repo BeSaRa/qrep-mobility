6990b1466e41d82d0b00facb719edef0650f00d7
eyad.alsawah <eyad.alsawah@nasca-sy.com>
2023-10-26 10:20:57 +0300

    merge

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,19 +8,13 @@ import Foundation
 import connectivity_plus
 import path_provider_foundation
 import shared_preferences_foundation
-<<<<<<< HEAD
-import url_launcher_macos
-=======
 import sqflite
->>>>>>> main
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-<<<<<<< HEAD
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-=======
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
->>>>>>> main
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
