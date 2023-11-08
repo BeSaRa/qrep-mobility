b9bc74ccc4fca7838436950d5a78e62d7cc2eb7f
Devops\fatina <apple@icloud.com>
2023-11-08 12:18:41 +0300

    add firebase

---
+++ android/app/src/main/kotlin/com/eblacorp/qrep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eblacorp.qrep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/ebla/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ebla
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
