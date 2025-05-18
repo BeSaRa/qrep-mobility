37b71779560a57c475ff9fde4050773e07544f92
M-ZakariaNa-K <zaokan225@gmail.com>
2025-05-18 16:26:29 +0300

    refactor constants code

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -23,30 +23,15 @@ class Constant {
   static const String webUrl = 'Https://qrepbe.aqarat.gov.qa/';
   static const String cmsBaseUrl = 'https://qrepcms.aqarat.gov.qa';
   static const String aqaratBaseUrl = 'https://www.aqarat.gov.qa';
-  //STG
-  // static const String authorityChatBotBaseUrl ='https://afnqcscbai01.azurewebsites.net';
-  //PRD
-  static const String authorityChatBotBaseUrl =
-      'https://apigatewaystg.aqarat.gov.qa/api-afnqcscbai01';
-  /*============= NOTE FROM ZAK To FATINA:==============
- ** u can find the xFunctions key from our chat in Teams
- */
-  static String ocpApimSubscriptionKey = "";
-
-  // static String xFunctionsAuthorityChatbotKey =  dotenv.env['X_FUNCTION_AUTHORITY_CHATBOT_KEY'] ?? 'NO_KEY_FOUND';
-  //============= NOTE FROM ZAK To FATINA:==============
-  // static String xFunctionsAuthorityChatbotKey =  ;
-  //1- uncomment last line:
-  //2- put the key I send in Teams in last line then run the app
-
+  //---------------------------------------------------------------------------------------------------------------
   // static const String baseUrl = 'http://192.168.52.4:9080/mme-services';
   // static const String webUrl = 'http://192.168.52.5:3000/';
   // static const String cmsBaseUrl = 'http://192.168.52.5:8055';
   //static const String aqaratBaseUrl = 'https://ministry-municipality.v2202305135856227727.ultrasrv.de';
   // static const String chatBotBaseUrl = 'https://afnqcpcbai01.azurewebsites.net';
 
-  static const String guestToken = "lyHWSTHj1SBm9IRECnLAHviNHnXGaS27";
   //---------------------------------------------------------------------------------------------------------------
+    static const String guestToken = "lyHWSTHj1SBm9IRECnLAHviNHnXGaS27";
   /*Authority Map Points*/
   static const double latitude = 25.370494;
   static const double longitude = 51.552943;
@@ -56,4 +41,12 @@ class Constant {
       "https://www.google.com/maps/place/%D8%A7%D9%84%D9%87%D9%8A%D8%A6%D8%A9+%D8%A7%D9%84%D8%B9%D8%A7%D9%85%D8%A9+%D9%84%D8%AA%D9%86%D8%B8%D9%8A%D9%85+%D8%A7%D9%84%D9%82%D8%B7%D8%A7%D8%B9+%D8%A7%D9%84%D8%B9%D9%82%D8%A7%D8%B1%D9%8A+-+%D8%B9%D9%82%D8%A7%D8%B1%D8%A7%D8%AA%E2%80%AD/@25.370463,51.552953,777m/data=!3m1!1e3!4m6!3m5!1s0x3e45c30011f86e83:0x4dc04497efcca14c!8m2!3d25.3704625!4d51.5529531!16s%2Fg%2F11whwkhg57?hl=en&entry=ttu&g_ep=EgoyMDI1MDMxMC4wIKXMDSoASAFQAw%3D%3D";
   static String publicAccessToken =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJTZXJ2aWNlVXNlciIsInVzZXJuYW1lIjoiU2VydmljZVVzZXIiLCJkZXBhcnRtZW50IjpudWxsLCJtYW5hZ2VyX25hbWUiOm51bGwsImVtcGxveWVlX0lEIjpudWxsLCJyb2xlIjpudWxsLCJwZXJtaXNzaW9uc19pZHMiOlsiMSIsIjQiLCIyMiIsIjIzIiwiMjQiLCIyNSIsIjI3IiwiMjgiLCI0MiJdLCJleHAiOjE4NzQ1Njg3NTQsInRva2VuX3R5cGUiOiJhY2Nlc3MifQ.RQDCf82hHq0QKQTVa5XCO15PRgx6J-joLBa0py7vxJI";
+  //STG
+  // static const String authorityChatBotBaseUrl ='https://afnqcscbai01.azurewebsites.net';
+  //PRD
+  static const String authorityChatBotBaseUrl ='https://apigatewaystg.aqarat.gov.qa/api-afnqcscbai01';
+  ///============= NOTE FROM ZAK To FATINA:==============
+  ///u can find the ocpApimSubscription Key from our chat in Teams
+  static String ocpApimSubscriptionKey = "";
+
 }
